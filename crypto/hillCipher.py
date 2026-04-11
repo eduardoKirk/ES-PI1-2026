e89c90f6d51c7dbf1e8ad6c1b352993c1cef0afe
eduardo <eduardo.mc2@puccampinas.edu.br>
2026-04-11 20:19:50 -0300

    fazendo a descriptografia

--- crypto/hillCipher.py
+++ crypto/hillCipher.py
@@ -44,7 +44,7 @@ def transformaEmLetra(matriz):
     matrizNova = []
     for i in range(linhasM):
         for j in range(colunasM):
-            matrizNova.insert(matriz[i][j], alfabeto[matriz[i][j]])
+            matrizNova.append(alfabeto[matriz[i][j]])
     return matrizNova
 
 def transformaEmNumero(matriz):
@@ -86,6 +86,60 @@ def hillCipher(p, k):
     return result
 
 
-c = hillCipher('sixs', matrizA)
-c2 = hillCipher('even', matrizA)
-print(c, c2)
+c = hillCipher('abra', matrizA)
+
+print(c)
+
+def matrixAdj(m):
+    mAdj = []
+    for i in range(len(m)):
+        list = []
+        for j in range(len(m[0])):
+            list.append(0)
+        mAdj.append(list)
+    mAdj[0][0] = m[1][1]
+    mAdj[0][1] = int(-m[0][1]) % 26
+    mAdj[1][0] = int(-m[1][0]) % 26
+    mAdj[1][1] = m[0][0]
+
+    return mAdj
+
+def determinante(m):
+    det = (m[0][0] * m[1][1])-(m[0][1] * m[1][0])
+    return det
+
+def inversoDet(det):
+    a = [1, 3 ,5, 9, 11, 15, 17, 19, 21, 23]
+    aInverso = [1, 9, 21, 15, 3, 19, 7, 23, 11, 5, 17]
+    for i in range(len(a)):
+        if det == a[i]:
+            detInverso = aInverso[i]
+    return detInverso
+
+def matrizxnum(num, matriz):
+    linhas= len(matriz)
+    colunas = len(matriz[0])
+    for i in range(linhas):
+        for j in range(colunas):
+            matriz[i][j] = matriz[i][j] * num
+    return matriz
+
+def decifrar(k, c):
+    detK = determinante(k)
+    inversoDetK = inversoDet(detK)
+    kAdj = matrixAdj(k)
+    mult = matrizxnum(inversoDetK, kAdj)
+    kInverso = pmodulo(mult, 26)
+    
+    c = list(c.upper())
+    cNum = transformaEmNumero(c)
+    cNumT = transporMatriz(cNum)
+    p = matrixMultiplication(kInverso, cNumT)
+    pMod = pmodulo(p, 26)
+    palavra = transformaEmLetra(pMod)
+    palavra = [palavra[i:i+2] for i in range(0, len(palavra), 2)]
+    palavra = transporMatriz(palavra)
+    palavra = sum(palavra, [])
+    return palavra
+
+print(decifrar(matrizA, 'ijed'))
