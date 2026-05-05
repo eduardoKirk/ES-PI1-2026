225146405eefc9eb4d888f5a078d8a4b7e01d0c2
JoseGB3 <jose.gb3@puccampinas.edu.br>
2026-05-05 16:36:03 -0300

    função FecharVotacao

--- votação/functions/index.py
+++ votação/functions/index.py
@@ -9,6 +9,53 @@ from utils.utils import criptografaCPF, criptografaChave,descriptografaCPF, chav
 import gerenciamento.infra.database
 from crypto.hillCipher import *
 
+
+def FecharVotacao(conexao):
+    titulo_eleitor = input("Digite o titulo de eleitor: ")
+    cpf = input("Digite os primeiros 4 digitos do seu CPF: ")
+    chave_acesso = input("Digite a chave de acesso: ")
+ 
+    try:
+        chave_acesso_crypto = criptografaChave(chave_acesso, chave)
+        cursor = conexao.cursor(dictionary=True)
+        sql_busca = f"SELECT * FROM eleitores WHERE titulo_eleitor = '{titulo_eleitor}'"
+        cursor.execute(sql_busca)
+        eleitor = cursor.fetchone()
+    except Error as e:
+        print(e)
+        return
+ 
+    eleitor_cpf = descriptografaCPF(eleitor['cpf'], chave)
+ 
+    chave_ok = eleitor['chave_acesso'] == chave_acesso_crypto
+    cpf_ok = str(eleitor_cpf)[:4] == cpf
+
+if chave_ok and cpf_ok:
+    if eleitor['mesario'] == 1:
+        confirmar = input("Deseja realmente encerrar a votação? (Sim/Não): ")
+            confirmar = confirmar.lower()
+ 
+            if confirmar == 'sim':    
+ 
+                #segunda confirmacao da chave:
+                chave_confirmacao = input("Digite novamente sua chave de acesso para confirmar: ")
+                chave_confirmacao_crypto = criptografaChave(chave_confirmacao, chave)
+ 
+                if chave_confirmacao_crypto == eleitor['chave_acesso']:
+                    print("Votação encerrada com sucesso!")
+                    return 0  # VotacaoAberta = 0
+                else:
+                    print("Chave de acesso incorreta. Encerramento cancelado.")
+ 
+            else:
+                print("Encerramento cancelado. Voltando ao menu anterior.")
+ 
+        else:
+            print("Você não tem permissão para encerrar o sistema de votação\n\n")
+    else:
+        print("CPF ou chave de acesso inválidos\n\n")
+
+
 def menu():
     a = 0
     while not a == 6:
@@ -25,6 +72,7 @@ def menu():
                 #resultados
             case 4:
                 print("\n")
+                FecharVotacao(gerenciamento.infra.database.conexao)
                 #FecharVotacao
                 break
             case _:
